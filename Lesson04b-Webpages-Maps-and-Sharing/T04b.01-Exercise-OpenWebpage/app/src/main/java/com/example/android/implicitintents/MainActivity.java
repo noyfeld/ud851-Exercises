95a68cfcf1195a859ba73acc7f27b349d8de3f2e
Noy Feld <noyfeld@hotmail.com>
2017-07-08 22:52:52 +0300

    lesson 4 completed

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,12 +15,17 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
         // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
 
         // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        String siteAddress = "https://www.udacity.com";
+        openWebPage(siteAddress);
     }
 
     /**
@@ -48,7 +55,9 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMapAddress("Kyiv Boryspil International Airport, Boryspil', Ukraine");
+
     }
 
     /**
@@ -58,7 +67,8 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickShareTextButton(View v) {
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        shareText("hi dear another programs activity");
     }
 
     /**
@@ -66,23 +76,67 @@ public class MainActivity extends AppCompatActivity {
      * similar to what I've done above. You can view a list of implicit Intents on the Common
      * Intents page from the developer documentation.
      *
-     * @see <http://developer.android.com/guide/components/intents-common.html/>
-     *
      * @param v Button that was clicked.
+     * @see <http://developer.android.com/guide/components/intents-common.html/>
      */
     public void createYourOwn(View v) {
-        Toast.makeText(this,
+        /*Toast.makeText(this,
                 "TODO: Create Your Own Implicit Intent",
                 Toast.LENGTH_SHORT)
-                .show();
+                .show();*/
+        dialPhoneNumber("0999999999");
     }
 
     // TODO (1) Create a method called openWebPage that accepts a String as a parameter
     // Do steps 2 - 4 within openWebPage
 
-        // TODO (2) Use Uri.parse to parse the String into a Uri
+    // TODO (2) Use Uri.parse to parse the String into a Uri
 
-        // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+    // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
 
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
+    // TODO (4) Verify that this Intent can be launched and then call startActivity
+    void openWebPage(String siteAddress) {
+        Uri siteUri = Uri.parse(siteAddress);
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, siteUri);
+
+        if (webIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(webIntent);
+        }
+    }
+
+    void showMapAddress(String streetAddress) {
+        Uri.Builder builder = new Uri.Builder();
+        Uri mapUri = builder.scheme("geo").path("0,0").query(streetAddress).build();
+        Log.d("mymymy", mapUri.toString());
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
+    void shareText(String sharedText) {
+        /*Intent textIntent = new Intent();
+        textIntent.setAction(Intent.ACTION_SEND);
+        textIntent.putExtra(Intent.EXTRA_TEXT, sharedText);
+        textIntent.setType("text/plain");
+
+
+
+        startActivity(Intent.createChooser(textIntent, "share text via..."));*/
+        Intent sharedIntent = ShareCompat.IntentBuilder.from(this).setType("text/*").setChooserTitle("share text via...")
+                .setText(sharedText).getIntent();
+        startActivity(sharedIntent);
+
+    }
+
+    void dialPhoneNumber(String phoneNumber) {
+        Uri phoneUri = new Uri.Builder().scheme("tel").path("+xyzb").build();
+        Log.d("mymymy",phoneUri.toString());
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL, phoneUri);
+
+        if (dialIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(dialIntent);
+        }
+    }
 }
